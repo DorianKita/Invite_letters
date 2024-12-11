cf07325f1a9d604e7aa2744d75cf64388f42c711
Dorian Kita <dorian.kita90@gmail.com>
2024-12-11 14:25:51 +0100

    refactored code to be simpler

--- main.py
+++ main.py
@@ -1,36 +1,15 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+PLACEHOLDER = "[name]"
 
-with open('./Input/Names/invited_names.txt') as file:
-    clear_names = []
-    names = file.readlines()
+
+with open('./Input/Names/invited_names.txt') as names_file:
+    names = names_file.readlines()
+
+with open('./Input/Letters/starting_letter.txt') as letter_file:
+    letter_contents = letter_file.read()
 
     for name in names:
-        new_name = name.strip()
-        clear_names.append(new_name)
+        stripped_name = name.strip()
+        new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
 
-
-
-
-for name in clear_names:
-    letter = open('./Input/Letters/starting_letter.txt')
-
-    changed_letter = letter.readlines()
-
-    with open(f'./Output/ReadyToSend/{name}_invitation.txt',mode='w') as letter:
-        for change in changed_letter:
-            new_letter = change.replace('[name]',name)
-            letter.write(new_letter)
-
-
-
-    # new_letter = open(f'./Output/ReadyToSend/{name}_invitation.txt',mode='w')
-
-
-
+        with open(f"Output/ReadyToSend/letter_for_{stripped_name}.txt", mode='w') as completed_letter:
+            completed_letter.write(new_letter)
