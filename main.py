b65968c71751681f24b4f647b6f81f0a0a6617ac
Dorian Kita <dorian.kita90@gmail.com>
2024-12-11 14:08:47 +0100

    created automation for creating invitation letters

--- main.py
+++ main.py
@@ -6,3 +6,31 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+with open('./Input/Names/invited_names.txt') as file:
+    clear_names = []
+    names = file.readlines()
+
+    for name in names:
+        new_name = name.strip()
+        clear_names.append(new_name)
+
+
+
+
+for name in clear_names:
+    letter = open('./Input/Letters/starting_letter.txt')
+
+    changed_letter = letter.readlines()
+
+    with open(f'./Output/ReadyToSend/{name}_invitation.txt',mode='w') as letter:
+        for change in changed_letter:
+            new_letter = change.replace('[name]',name)
+            letter.write(new_letter)
+
+
+
+    # new_letter = open(f'./Output/ReadyToSend/{name}_invitation.txt',mode='w')
+
+
+
